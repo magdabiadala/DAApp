3d1e9b0fe9efab929b3ad47a37a98b7374c1ea86
magdabiadala <magdalena.biadala@gmail.com>
2019-11-22 14:29:29 +0100

    zmiana kodu podstrony data analisys

--- uploads/core/views.py
+++ uploads/core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from scipy import stats
+from string import ascii_letters
 
 import csv
 import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 
 def home(request):
@@ -51,8 +55,29 @@ def data_analysis(request):
                 print(row)
 
         print('Data analysis')
-        r_table = df.apply(lambda x: df.apply(lambda y: r_xor_p(x, y,
-                                                                r_xor_p='r')))
+
+        sns.set(style="white")
+
+        corr = df.corr()
+
+        # Generate a mask for the upper triangle
+        mask = np.zeros_like(corr, dtype=np.bool)
+        mask[np.triu_indices_from(mask)] = True
+
+        # Set up the matplotlib figure
+        f, ax = plt.subplots(figsize=(11, 9))
+
+        # Generate a custom diverging colormap
+        cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+        # Draw the heatmap with the mask and correct aspect ratio
+        matrix = sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+                    square=True, linewidths=.5, cbar_kws={"shrink": .5})
+
+
+        matrix.figure.savefig(r'C:\Users\Magda\Desktop\kogni\KCK\DAApp\static\img\matrix.png')
+
+        r_table = df.corr()
         p_table = df.apply(lambda x: df.apply(lambda y: r_xor_p(x, y,
                                                                 r_xor_p='p')))
 
@@ -66,9 +91,8 @@ def data_analysis(request):
 
 
 def r_xor_p(x, y, r_xor_p='r'):
-    ''' Pearson's r or its p
-    Depending of what you would like to get.
-    '''
+# Pearson's r or its p
+#    Depending of what you would like to get.
     r, p = stats.pearsonr(x, y)
 
     if r_xor_p == 'r':
